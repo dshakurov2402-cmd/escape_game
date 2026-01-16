974c9f652caac128dad3921694cf79f4b494bfd0
Nail Shakurov <nshakurov@gmail.com>
2026-01-16 16:35:32 +0500

    ффикс

--- assets/level2.js
+++ assets/level2.js
@@ -1,28 +1,24 @@
-// assets/level2.js
-
-// Это закодированный флаг "FLAG{CONSOLE_HACKER_2026}"
-const _0x4f22 = "RkxBR3tDT05TT0xFX0hBQ0tFUl8yMDI2fQ=="; 
+// Код для файла assets/level2.js
+const encodedFlag = "RkxBR3tDT05TT0xFX0hBQ0tFUl8yMDI2fQ=="; 
+const secret = atob(encodedFlag);
 
 console.log("%c--- СИСТЕМНОЕ СООБЩЕНИЕ ---", "color: orange; font-weight: bold; font-size: 16px;");
 console.log("Доступ к уровню 3 заблокирован.");
-console.log("Ключ дешифрации: " + atob(_0x4f22)); // atob расшифрует это только в консоли
-console.log("---------------------------");
+console.log("Ключ дешифрации: " + secret);
 
 const input = document.getElementById('flagInput');
 const button = document.getElementById('nextLevelBtn');
 
 input.addEventListener('input', () => {
-    // Проверяем ввод, сравнивая его с расшифрованным значением
-    if (input.value.trim() === atob(_0x4f22)) {
+    if (input.value.trim() === secret) {
         button.disabled = false;
         button.style.backgroundColor = "#28a745";
     } else {
         button.disabled = true;
-        button.style.backgroundColor = "#007bff";
     }
 });
 
 button.addEventListener('click', () => {
-    alert('Поздравляю! Ты взломал уровень папы! \n\nТы крутышка!.\n\nТеперь ТВОЙ ХОД:\n1. Создай файл levels/level3.html\n2. Придумай свою загадку для меня!');
+    alert('Поздравляю! Ты взломал уровень папы!');
     window.location.href = "../index.html";
 });
